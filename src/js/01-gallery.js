88df76a36b0f73fead4190b24de5f91ef0d46a55
Miradzyapko <mira.dzyapko@gmail.com>
2022-10-13 01:57:29 +0300

    add files

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+const imagesMarkup = createImagesMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", imagesMarkup);
+//console.log(galleryContainer)//
+
+function createImagesMarkup(galleryItems) {
+return galleryItems.map(({ preview, original, description }) => {
+    return `<div class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"/></a></div>`;
+}).join('');
+}
+
+
+    const lightbox = new SimpleLightbox(".gallery a", {
+
+        captionsData: "alt",
+        captionDelay: 250,
+        captionPosition: "bottom",
+    })
+
